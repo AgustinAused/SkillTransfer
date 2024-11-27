5f1172bf2a875f95be5c4a631905f0d08e854f50
Agustin Aused <agus.aused@gmail.com>
2024-11-27 20:17:51 -0300

    Primer commit: Subida inicial del proyecto

---
+++ src/main/java/com/skilltranfer/skilltrafer/SkillTraferApplication.java
@@ -0,0 +1,13 @@
+package com.skilltranfer.skilltrafer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillTraferApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkillTraferApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skilltranfer/skilltrafer/SkillTraferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilltranfer.skilltrafer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillTraferApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
